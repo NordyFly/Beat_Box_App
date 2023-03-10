c8a14169caf4ae3612b0efc375f0e6b7502b69e6
NordyFly <benomar886@outlook.fr>
2023-03-10 09:01:37 +0000

    test de commit

---
+++ index.js
@@ -0,0 +1,37 @@
+var bg = document.getElementById('particles-js');
+document.addEventListener('keydown',function(event){
+    bg.style.backgroundColor = getRandomColor();
+ var key =event.key;
+ playMusic(key);
+ console.log(event);
+});
+function playMusic(key){
+switch (key) {
+    case "a":
+        var audio = new Audio ("test1.mp3");
+       audio.play();
+       break;
+       case "z":
+        var audio = new Audio ("kick.mp3");
+       audio.play();
+       break;
+       case "e":
+        var audio = new Audio ("kick2.mp3");
+       audio.play();
+       break;
+       case "r":
+        var audio = new Audio ("clap1.mp3");
+       audio.play();
+       break;
+}
+}
+//Une fonction pour générer une couleur HEX aléatoire 
+function getRandomColor(){
+var letters ="0123456789ABCDEF";
+var color = "#";
+for (var i=0; i<6;i++) {
+   color = color + letters[Math.floor(Math.random()*letters.length)]
+    
+}
+return color;
+}
